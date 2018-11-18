dbeb6f8ffdd81bd0271e1343721a0811caa23a63
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2018-11-18 15:36:31 -0500

    Logica del programa

--- codebreaker.js
+++ codebreaker.js
@@ -1,9 +1,31 @@
-var realNumber = "1234";
+var realNumber = "5612";
 
- const guess = function(number) {
-  // TODO: Logic Here
+const guess = function(number) {
+  let realNumberArr = realNumber.split("")
+  let numberArr = number.split("")
 
-  return "XXXX";
+  var resultado = ''
+  var existe = false
+
+
+  for(let i = 0; i < numberArr.length; i++){
+    existe = false
+    for(let j = 0; j < realNumberArr.length; j++){
+      if(numberArr[i] === realNumberArr[j]){
+        existe = true
+        if(i === j){
+          resultado =  resultado + 'X'
+        }
+        else {
+          resultado = resultado + '0'
+        }
+      }
+    }
+    if (existe === false) {
+      resultado = resultado + '-'
+    }
+  }
+
+  return resultado;
 }
-
 module.exports = guess;
