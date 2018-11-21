68ea20e53824004f04f44510aa9f5388f9d58132
Lizeth Victoria Franco <lizvictoria754@gmail.com>
2018-11-21 15:54:51 -0500

    Pruebas con letras

--- codebreaker.js
+++ codebreaker.js
@@ -8,6 +8,27 @@ const guess = function(number) {
   var existe = false
 
 
+  //PRUEBAS CON MAS O MENOS DE 4 NUMEROS
+  if(number.length < 4){
+    resultado = resultado + 'MENOS DE 4 NUMEROS🚫'
+    return resultado
+  }
+  if(number.length > 4){
+    resultado = resultado + 'MAS DE 4 NUMEROS🚫'
+    return resultado
+  }
+
+  //PRUEBAS CON LETRAS
+   for(let posicion = 0; posicion < number.length; posicion++){
+     var valorDecimal = number.charCodeAt(posicion) //Valor decimal del caracter
+      if( valorDecimal < 48 || valorDecimal > 57 ){
+        resultado = resultado + 'ERROR'
+        return resultado
+      }
+   }
+
+
+
   for(let i = 0; i < numberArr.length; i++){
     existe = false
     for(let j = 0; j < realNumberArr.length; j++){
